37ea178de30b348f8462c8ae6c73f6ce61f8a449
Miguel Cruz <mlpcs@cin.ufpe.br>
2020-11-09 19:29:11 -0300

    q1,2

--- 2020-3/lab/lab3/lab3.py
+++ 2020-3/lab/lab3/lab3.py
@@ -1,21 +1,37 @@
 import qiskit
+import numpy as np
 
 def inv_media(vetor):
-    inv_media_vetor = 0
+    mean = np.mean(vetor)
+    inv_media = [2*mean - elem for elem in vetor]
     return inv_media
 
 def qinv_media(n):
-    '''
-    :param n: número de qubits
-    :return: circuit inversão sobre a média
-    '''
-    circuito = qiskit.QuantumCircuit(n)
+  
+    q = qiskit.QuantumRegister(n, name = 'q')
+    circuito = qiskit.QuantumCircuit(q)
+
+    for qu in range(n):
+        circuito.h(qu)
+
+    for qu in range(n):
+        circuito.x(qu)
+    
+    circuito.h(n-1)
+    circuito.mct(list(range(n-1)), n-1)
+    circuito.h(n-1)
+
+    for qu in range(n):
+        circuito.x(qu)
+
+    for qu in range(n):
+        circuito.h(qu)
     return circuito
 
 def oraculo_trivial(n, k):
-    
+    circuito_oraculo = {}
     return circuito_oraculo
 
 def grover(oraculo):
-    
+    k = {}
     return k
